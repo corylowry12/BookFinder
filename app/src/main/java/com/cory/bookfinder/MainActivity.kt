4d55c656ab14035d6b45bc495dd39ab83df3d6a1
corylowry12 <corylowry12@gmail.com>
2021-04-10 04:39:51 -0500

    Minor Changes

--- app/src/main/java/com/cory/bookfinder/MainActivity.kt
+++ app/src/main/java/com/cory/bookfinder/MainActivity.kt
@@ -1,129 +1,68 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.cory.bookfinder
 
-import android.content.Context
-import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.gms.ads.*
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var firebaseAnalytics: FirebaseAnalytics
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        val themeData = ThemeData(this)
-        if(themeData.loadDarkModeState()) {
-            setTheme(R.style.AMOLED)
-        }
-        else {
-            setTheme(R.style.AppTheme)
-        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapper = jacksonObjectMapper()
 
-        MobileAds.initialize(this)
-        val adView = AdView(this)
-        adView.adSize = AdSize.BANNER
-        adView.adUnitId = "ca-app-pub-4546055219731501/1995733094"
-        val mAdView = findViewById<AdView>(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
-        mAdView.adListener = object : AdListener() {
+        //val url = mapper.readValue<URL>("https://bookpage.com/")
+        //val url = URL("https://bookpage.com/").readText()
+        /*val url = Retrofit.Builder()
+            .baseUrl("https://bookpage.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()*/
 
-        }
+        //Toast.makeText(this, url.toString(), Toast.LENGTH_SHORT).show()
 
-        firebaseAnalytics = Firebase.analytics
+        val url = "https://duckduckgo.com/?q=book+website+that+uses+json&t=brave&ia=web&iai=r1-5&page=1&sexp=%7B%22cdrexp%22%3A%22b%22%2C%22prodexp%22%3A%22b%22%2C%22prdsdexp%22%3A%22c%22%2C%22biaexp%22%3A%22b%22%2C%22msvrtexp%22%3A%22b%22%7D"
 
-        textView4.visibility = View.INVISIBLE
+        JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+                textView.text = "Response: %s".format(response.toString())
+            },
+            { error ->
+                Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show()
+            })
 
-        button.setOnClickListener {
-            if (editText.text.toString() == "") {
-                textView4.visibility = View.VISIBLE
-                textView4.text = getString(R.string.cant_be_thinking)
-            } else {
-                val isOnlineFun = isOnline(this)
-                if (isOnlineFun) {
-                    val i = Intent(this, SearchResults::class.java)
-                    i.putExtra("whats on your mind", editText.text.toString())
-                    startActivity(i)
-                }
-                else {
-                    Toast.makeText(this, "You have no connection, please try again", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-        val constraintLayout = findViewById<ConstraintLayout>(R.id.constraintLayout)
-        constraintLayout.setOnClickListener {
-            if (editText.hasFocus())
-                hideKeyboard()
-            editText.clearFocus()
-        }
-        button2.setOnClickListener {
-            editText?.text?.clear()
-        }
     }
+}
 
-    private fun isOnline(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if(capabilities != null) {
-            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
-                return true
-            }
-            else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
-                return true
-            }
-        }
-        return false
-    }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.toggle_dark_mode -> {
-                val themeData = ThemeData(this)
-                if(!themeData.loadDarkModeState()) {
-                    themeData.setDarkModeState(true)
-                    restartApplication()
-                }
-                else {
-                    themeData.setDarkModeState(false)
-                    restartApplication()
-                }
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-    private fun restartApplication() {
-        val intent = Intent(this, MainActivity::class.java)
-        this.startActivity(intent)
-        this.finishAffinity()
-    }
-    private fun hideKeyboard() {
-        val inputManager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        if (inputManager.isAcceptingText) {
-            inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.SHOW_FORCED)
-        }
-        if (editText.hasFocus()) {
-            editText.clearFocus()
-        }
-    }
+interface Api {
+    @GET("/top.json")
+    fun getTop(@Query("after") after: String, @Query("limit") limit: String): Call<String>
 }
